849f23c4634c46c06113ee9a9c50d9e179d9ea66
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2013-09-13 19:40:56 -0400

    local update

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cd ~/compil/llvm/llvm/clang/bindings/
+cd ~/compil/llvm/llvm/tools/clang/bindings/
 COMMIT=`git log | head -n 1`
 
 cd ~/compil/python-clang
 
-cp -a ~/compil/llvm/llvm/clang/bindings/python/clang .
-cp -a ~/compil/llvm/llvm/clang/bindings/python/examples .
-cp -a ~/compil/llvm/llvm/clang/bindings/python/tests .
+cp -a ~/compil/llvm/llvm/tools/clang/bindings/python/clang .
+cp -a ~/compil/llvm/llvm/tools/clang/bindings/python/examples .
+cp -a ~/compil/llvm/llvm/tools/clang/bindings/python/tests .
 
 git add clang examples tests
 git commit -m "$COMMIT"

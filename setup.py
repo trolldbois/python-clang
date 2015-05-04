466faef9abd9673f9807040a0529bfebd458b2d8
Loic Jaquemet <loic.jaquemet+python@gmail.com>
2015-05-04 16:09:38 -0400

    update setup.py to current clang version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name="clang",
-      version="3.4.dev192547",
+      version="3.7.dev234762",
       description="libclang python bindings",
       long_description=open("README.txt").read(),
       url="https://github.com/trolldbois/python-clang",

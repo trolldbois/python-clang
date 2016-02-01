a6f4344e9711e2dd5a3e6c51ce01b76bd9703065
Russell Keith-Magee <russell@keith-magee.com>
2016-02-01 11:01:31 +0800

    Added a setup.py file so that the Python bindings can be  installed.
    I would encourage the LLVM maintainers to use this setup.py
    to register the package on PyPI, as is the convention for Python tools.

--- setup.py
+++ setup.py
@@ -3,34 +3,32 @@
 
 from setuptools import setup
 
-setup(name="clang",
-      version="3.7.dev234765",
-      description="libclang python bindings",
-      long_description=open("README.txt").read(),
-      url="https://github.com/trolldbois/python-clang",
-      download_url="https://github.com/trolldbois/python-clang/releases",
-      license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
-      classifiers=[
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Development Status :: 5 - Production/Stable",
-          "Topic :: Software Development :: Compilers"
-      ],
-      keywords=["llvm", "clang", "libclang"],
-      author="Loic Jaquemet",  # meeeh... not.
-      author_email="loic.jaquemet+python@gmail.com",
-      zip_safe=False,
-      packages=["clang"],
-      # use pip requirements.txt instead
-      # install_requires = ["libclang"],
-      # build_tests_requires = ["libclang"],
-      # export LD_LIBRARY_PATH=$LLVM_LOCAL_REPO/build/Debug+Asserts/lib/
-
-      # if use nose.collector, many plugins not is avaliable
-      # see: http://nose.readthedocs.org/en/latest/setuptools_integration.html
-      test_suite="nose.collector",
-      tests_require=['nose']
+setup(
+    name="clang",
+    version="3.7.dev257739",
+    description="libclang python bindings",
+    long_description=open("README.txt").read(),
+    url="http://clang.llvm.org/",
+    download_url="http://llvm.org/releases/download.html",
+    license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved ::  University of Illinois/NCSA Open Source License",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Compilers"
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=["llvm", "clang", "libclang"],
+    author="LLVM team",
+    zip_safe=False,
+    packages=["clang"],
+    # if use nose.collector, many plugins not is avaliable
+    # see: http://nose.readthedocs.org/en/latest/setuptools_integration.html
+    test_suite="nose.collector",
+    tests_require=['nose']
 )
-
-

9140a21420f8333b47d74c5d2479927a9626d01c
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2017-04-30 22:22:09 -0600

    release 3.9

--- setup.py
+++ setup.py
@@ -3,34 +3,29 @@
 
 from setuptools import setup
 
-setup(name="clang",
-      version="3.8",
-      description="libclang python bindings",
-      long_description=open("README.txt").read(),
-      url="https://github.com/llvm-mirror/clang/bindings/python",
-      #download_url="https://github.com/trolldbois/python-clang/releases",
-      license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
-      classifiers=[
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Development Status :: 5 - Production/Stable",
-          "Topic :: Software Development :: Compilers"
-      ],
-      keywords=["llvm", "clang", "libclang"],
-      author="Loic Jaquemet",  # meeeh... not.
-      author_email="loic.jaquemet+python@gmail.com",
-      zip_safe=False,
-      packages=["clang"],
-      # use pip requirements.txt instead
-      # install_requires = ["libclang"],
-      # build_tests_requires = ["libclang"],
-      # export LD_LIBRARY_PATH=$LLVM_LOCAL_REPO/build/Debug+Asserts/lib/
-
-      # if use nose.collector, many plugins not is avaliable
-      # see: http://nose.readthedocs.org/en/latest/setuptools_integration.html
-      test_suite="nose.collector",
-      tests_require=['nose']
+setup(
+    name="clang",
+    version="3.9",
+    description="libclang python bindings",
+    long_description=open("README.txt").read(),
+    url="http://clang.llvm.org/",
+    download_url="http://llvm.org/releases/download.html",
+    license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Compilers",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=["llvm", "clang", "libclang"],
+    author="LLVM team",
+    zip_safe=False,
+    packages=["clang"],
+    # if use nose.collector, many plugins not is avaliable
+    # see: http://nose.readthedocs.org/en/latest/setuptools_integration.html
+    test_suite="nose.collector",
+    tests_require=['nose']
 )
-
-

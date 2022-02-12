3ae530a0ae3044339ffd2934a6e21800e7aeec00
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2022-02-12 11:06:31 -0700

    update for build, twine and clang 12, 13, 14

--- make-from-packages.py
+++ make-from-packages.py
@@ -7,7 +7,7 @@ import subprocess
 import requests
 
 
-class PackageBuilder():
+class PackageBuilder:
     code_folder = 'clang'
     temp_folder = 'tmp'
     temp_package_filename = 'tmp_llvm_clang.deb'
@@ -15,28 +15,31 @@ class PackageBuilder():
     setup_file = 'setup.py'
     setup_file_tpl = 'setup.py.tpl'
 
-    def do(self, version, pyversion, url):
-        version = str(version.strip())
-        pyversion = str(int(pyversion.strip()))
-        url = str(url.strip())
-        logging.info(f'packaging version {version} from {url}')
-        if pyversion == '2':
+    def __init__(self, version, pyversion, url):
+        self.version = version.strip()
+        self.pyversion = str(int(pyversion.strip()))
+        self.url = url.strip()
+
+    def do(self):
+        logging.info(f'packaging version {self.version} from {self.url}')
+        if self.pyversion == '2':
             self.python_version = 'python2.7'
         else:
             self.python_version = 'python3'
         self.clean()
-        self.download(url)
+        self.download()
         self.unpack()
         try:
             self.move()
         except FileNotFoundError as e:
-            logging.error(f"error in package v:{version} pyv:{pyversion} url:{url}")
+            logging.error(f"error in package v:{self.version} pyv:{self.pyversion} url:{self.url}")
             raise RuntimeError(
-                f"Package python-clang version {version} for python {pyversion} does "
+                f"Package python-clang version {self.version} for python {self.pyversion} does "
                 f"not contain clang python binding.") from e
-        self.setup(version, pyversion)
-        self.build_and_publish()
-        logging.info(f'packaged version {version} for python {pyversion}')
+        self.setup()
+        self.build()
+        self.upload()
+        logging.info(f'packaged version {self.version} for python {self.pyversion}')
 
     def clean(self):
         for d in [self.code_folder, self.temp_folder]:
@@ -46,8 +49,8 @@ class PackageBuilder():
             os.remove(self.setup_file)
         logging.debug("Working folders removed")
 
-    def download(self, url):
-        response = requests.get(url)
+    def download(self):
+        response = requests.get(self.url)
         logging.debug(f"download ret {response.status_code}")
         if response.status_code != 200:
             raise RuntimeError("download failed")
@@ -59,11 +62,12 @@ class PackageBuilder():
         return filename
 
     def unpack(self):
-        ret = subprocess.call(['dpkg-deb', '-xv', self.temp_package_filename, '.'], cwd=self.temp_folder,
-                              stdout=subprocess.DEVNULL)
+        ret = subprocess.run(['dpkg-deb', '-xv', self.temp_package_filename, '.'], cwd=self.temp_folder,
+                              capture_output=True)
         logging.debug(f"unpack ret {ret}")
-        if ret < 0:
-            raise RuntimeError("unpack failed")
+        if ret.returncode != 0:
+            logging.error(ret.stderr)
+            raise RuntimeError("unpack failed: ")
 
     def move(self):
         src = os.path.join(self.temp_folder, 'usr', 'lib', self.python_version, 'dist-packages', 'clang')
@@ -71,33 +75,43 @@ class PackageBuilder():
         shutil.move(src, dst)
         logging.debug(f"python package moved {src} -> {dst}")
 
-    def setup(self, version, pyversion):
+    def setup(self):
         with open(self.setup_file_tpl, 'r') as tpl:
             tpl_content = tpl.read()
-        tpl_content = tpl_content.replace('%VERSION%', version)
-        tpl_content = tpl_content.replace('%PYTHON_VERSION%', pyversion)
+        tpl_content = tpl_content.replace('%VERSION%', self.version)
+        tpl_content = tpl_content.replace('%PYTHON_VERSION%', self.pyversion)
         with open(self.setup_file, 'w') as setup_file:
             setup_file.write(tpl_content)
-        logging.debug(f"setup.py file writen for clang v:{version}, pyv:{pyversion}")
+        logging.debug(f"setup.py file writen for clang v:{self.version}, pyv:{self.pyversion}")
 
-    def build_and_publish(self):
-        ret = subprocess.call(['python', 'setup.py', 'sdist', 'bdist_wheel', 'upload'], stdout=subprocess.DEVNULL)
-        logging.debug(f"build_and_publish ret {ret}")
-        if ret < 0:
-            raise RuntimeError("build_and_publish failed")
+    def build(self):
+        # https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html
+        ret = subprocess.run(['python', '-m', 'build'], capture_output=True)
+        logging.debug(f"build ret {ret}")
+        if ret.returncode != 0:
+            logging.error(ret.stderr)
+            raise RuntimeError("build failed")
+
+    def upload(self):
+        # https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html
+        ret = subprocess.run(['twine', 'upload', f'dist/clang-{self.version}*'], capture_output=True)
+        logging.debug(f"upload ret {ret}")
+        if ret.returncode != 0:
+            logging.error(ret.stderr)
+            raise RuntimeError("upload failed")
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    builder = PackageBuilder()
     with open('packages.lst', newline='') as csvfile:
         reader = csv.reader(csvfile)
         headers = next(reader)
         for version, pyversion, url in reader:
             if version.startswith('#'):
                 continue
-            builder.do(version, pyversion, url)
+            builder = PackageBuilder(version, pyversion, url)
+            builder.do()
 
 
 if __name__ == '__main__':
